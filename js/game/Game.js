44058a563e09b8247e3d973d42d2a03a12808394
Emanuel Palm <emanuelpalm@gmail.com>
2014-11-06 11:34:40 +0100

    Added GameCanvas to Game class.

--- js/game/Game.js
+++ js/game/Game.js
@@ -3,6 +3,7 @@
 
   var GameLooper = require("./GameLooper.js");
   var GameSceneProxy = require("./GameSceneProxy.js");
+  var GameCanvas = require("./GameCanvas.js");
 
   /**
    * Represents the entirety of the game.
@@ -12,6 +13,7 @@
   function Game(properties) {
     properties.looper = new GameLooper();
     properties.sceneProxy = new GameSceneProxy(properties.scene);
+    properties.gameCanvas = new GameCanvas(properties.$canvas);
 
     this._getProperties = function () {
       return properties;
@@ -24,10 +26,11 @@
   Game.prototype.start = function () {
     var p = this._getProperties();
 
+    this.resize(p.$canvas.offsetWidth, p.$canvas.offsetHeight);
     p.looper.loop(function (dt) {
       p.sceneProxy.update(dt);
       p.sceneProxy.render(null); // TODO: Pass on camera object.
-
+      p.gameCanvas.render(null); // TODO: Pass on camera object.
       // TODO: Anything else? Handle events?
     });
   };
@@ -38,6 +41,16 @@
   Game.prototype.stop = function () {
     this._getProperties().looper.stop();
   }
+
+  /**
+   * Resizes game canvas.
+   *
+   * @param  {integer} width  - Target width, in pixels.
+   * @param  {integer} height - Target height, in pixels.
+   */
+  Game.prototype.resize = function (width, height) {
+    this._getProperties().gameCanvas.resize(width, height);
+  };
 
   module.exports = Game;
 }());
