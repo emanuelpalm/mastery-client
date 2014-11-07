ae03f40c592551bcb3f170f26a1ef17a64f3c333
Emanuel Palm <emanuelpalm@gmail.com>
2014-11-07 09:21:26 +0100

    Created canvas module for encapsulating canvas management.

---
+++ js/utils/canvas.js
@@ -0,0 +1,41 @@
+/**
+ * Contains helper functions for creating and managing HTMLCanvasElements.
+ *
+ * @module utils/canvas-element
+ */
+(function () {
+  "use strict";
+
+  /**
+   * Creates new canvas element.
+   *
+   * @param  {integer} width - Canvas width in pixels.
+   * @param  {integer} height - Canvas height in pixels.
+   * @return {HTMLCanvasElement} Canvas element.
+   */
+  exports.createCanvasElement = isBrowser() ? function (width, height) {
+    var $canvas = document.createElement("canvas");
+    $canvas.width = width;
+    $canvas.height = height;
+    return $canvas;
+  } : function () {
+    return {};
+  };
+
+  /**
+   * Disabled image smoothing for given CanvasRenderingContext2D.
+   *
+   * @param  {CanvasRenderingContext2D } context - Target context.
+   */
+  exports.disableContextImageSmoothing = function (context) {
+    context.mozImageSmoothingEnabled = false;
+    context.oImageSmoothingEnabled = false;
+    context.webkitImageSmoothingEnabled = false;
+    context.msImageSmoothingEnabled = false;
+    context.imageSmoothingEnabled = false;
+  };
+
+  function isBrowser() {
+    return (typeof document !== "undefined");
+  }
+}());
