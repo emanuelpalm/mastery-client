a177e0e2b4322f67c2bf518fd255e70d293dd19a
Emanuel Palm <emanuelpalm@gmail.com>
2014-11-17 17:19:36 +0100

    Added unimplemented LoginScene.

---
+++ js/scene/LoginScene.js
@@ -0,0 +1,20 @@
+(function () {
+  "use strict";
+
+  function LoginScene(gameMode) {
+
+    this.setup = function (loader, toScene) {
+
+    };
+
+    this.update = function (dt) {
+
+    };
+
+    this.record = function (camera) {
+
+    };
+  }
+
+  module.exports = LoginScene;
+}());
