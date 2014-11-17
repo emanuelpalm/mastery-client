8683462380f367dfda54ee36ec9a090408627ec1
Emanuel Palm <emanuelpalm@gmail.com>
2014-11-17 22:34:56 +0100

    Started on implementation of GameAssetLoader.

--- js/game/GameAssetLoader.js
+++ js/game/GameAssetLoader.js
@@ -1,30 +1,48 @@
 (function () {
   "use strict";
 
-  var http = require("http");
+  var httpSync = require("../utils/http-sync.js");
 
   /**
    * Utility used for loading assets from files at some given root directory.
    *
    * @class
    */
-  function GameAssetLoader(root) {
+  function GameAssetLoader(rootPath) {
 
     /**
      * Loads asset batch at given path relative to asset root directory.
      *
-     * The given callback, onLoad, is fired when the batch has loaded with a
-     * map of its contents as argument. If loading batch fails an exception is
-     * thrown and the application is terminated.
+     * The given callback f is fired when the batch has loaded with a map of its
+     * contents as argument. if an error occurrs the callback is also fired, but
+     * with its first argument set to null and with a second argument containing
+     * the error.
      */
-    this.loadBatch = function (path, onLoad) {
-      http.get(root + path, function (result) {
-        console.log(result);
+    this.loadBatch = function (path, f) {
+      setTimeout(function () {
+        try {
+          var data = httpSync.get(rootPath + "asdf" + path);
+          console.log(data);
 
-      }).on("error", function (e) {
-        throw e;
-      });
+        } catch (e) {
+          f(null, e);
+        }
+      }, 0);
     };
+
+    function loadEntitiesIn(list) {
+      if (!list) {
+        return {};
+      }
+      var entities = {};
+      Object.keys(list).forEach(function (key) {
+        entities[key] = loadEntityAt(list[key]);
+      });
+    }
+
+    function loadEntityAt(path) {
+
+    }
   }
 
   module.exports = GameAssetLoader;
