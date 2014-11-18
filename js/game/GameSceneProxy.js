738a6b73828ba90615d29312fe6ed07e51d65155
Emanuel Palm <emanuelpalm@gmail.com>
2014-11-18 20:19:55 +0100

    GameAssetLoader doesn't deal with relative paths.

--- js/game/GameSceneProxy.js
+++ js/game/GameSceneProxy.js
@@ -9,7 +9,7 @@
    * @class
    */
   function GameSceneProxy(originScene) {
-    var loader = new GameAssetLoader("/assets/");
+    var loader = new GameAssetLoader();
 
     var eventCallback = null;
     var scene = null;
