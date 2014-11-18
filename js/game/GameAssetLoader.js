2a0e99be5f91847a92f0917d7222f866795fb5cd
Emanuel Palm <emanuelpalm@gmail.com>
2014-11-18 09:08:41 +0100

    Removed http-sync module.

--- js/game/GameAssetLoader.js
+++ js/game/GameAssetLoader.js
@@ -1,8 +1,6 @@
 (function () {
   "use strict";
 
-  var httpSync = require("../utils/http-sync.js");
-
   /**
    * Utility used for loading assets from files at some given root directory.
    *
@@ -13,36 +11,12 @@
     /**
      * Loads asset batch at given path relative to asset root directory.
      *
-     * The given callback f is fired when the batch has loaded with a map of its
-     * contents as argument. if an error occurrs the callback is also fired, but
-     * with its first argument set to null and with a second argument containing
-     * the error.
+     * The method returns a promise, which when done serves a map containing the
+     * batch data.
      */
-    this.loadBatch = function (path, f) {
-      setTimeout(function () {
-        try {
-          var data = httpSync.get(rootPath + "asdf" + path);
-          console.log(data);
-
-        } catch (e) {
-          f(null, e);
-        }
-      }, 0);
+    this.loadBatch = function (path) {
+      
     };
-
-    function loadEntitiesIn(list) {
-      if (!list) {
-        return {};
-      }
-      var entities = {};
-      Object.keys(list).forEach(function (key) {
-        entities[key] = loadEntityAt(list[key]);
-      });
-    }
-
-    function loadEntityAt(path) {
-
-    }
   }
 
   module.exports = GameAssetLoader;
