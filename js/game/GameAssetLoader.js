18d1a4e21b961608b0585e818d50b3e83fb6921b
Emanuel Palm <emanuelpalm@gmail.com>
2014-11-18 20:53:03 +0100

    Started on implementing GameAssetLoader.

--- js/game/GameAssetLoader.js
+++ js/game/GameAssetLoader.js
@@ -1,22 +1,76 @@
 (function () {
   "use strict";
 
+  var Promise = require("promise");
+  var http = require("http");
+
   /**
-   * Utility used for loading assets from files at some given root directory.
+   * Utility class used for loading assets.
    *
    * @class
    */
-  function GameAssetLoader(rootPath) {
+  function GameAssetLoader() {
+    var imageRegex = /.(png|jpg|jpeg|bmp)$/i;
+    var cache = {};
 
     /**
-     * Loads asset batch at given path relative to asset root directory.
+     * Loads asset batch at given URL.
      *
      * The method returns a promise, which when done serves a map containing the
      * batch data.
      */
-    this.loadBatch = function (path) {
-      
+    this.loadBatch = function (url) {
+      return new Promise(function (fulfill, reject) {
+        loadOne(url)
+          .then(function (batch) {
+            console.log(batch);
+          })
+          .catch(reject);
+      });
     };
+
+    function loadOne(url) {
+      var cached = cache[url];
+      if (cached) {
+        return new Promise(function (fulfill) {
+          fulfill(cached);
+        });
+      }
+
+      if (url.match(imageRegex)) {
+        return new Promise(function (fulfill, reject) {
+          var image = new Image();
+          image.addEventListener("load", function () {
+            cache[url] = image;
+            fulfill(image);
+          });
+          image.addEventListener("error", function () {
+            reject(new Error("Unable to load image at '" + url + "'."));
+          });
+          image.src = url;
+        });
+      }
+
+      return new Promise(function (fulfill, reject) {
+        http.request(url)
+          .on("response", function (response) {
+            response.on("data", function (data) {
+              if (response.headers["content-type"] === "application/json") {
+                data = JSON.parse(data);
+                Object.freeze(data);
+              }
+              if (response.statusCode < 200 || response.statusCode > 299) {
+                reject(data);
+                return;
+              }
+              cache[url] = data;
+              fulfill(data);
+            });
+          })
+          .on("error", reject)
+          .end();
+      });
+    }
   }
 
   module.exports = GameAssetLoader;
