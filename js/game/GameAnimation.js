220e888b93093956d62b690e66434eb486f740ec
Emanuel Palm <emanuelpalm@gmail.com>
2014-11-24 09:34:18 +0100

    Started on GameAnimation implementation.

--- js/game/GameAnimation.js
+++ js/game/GameAnimation.js
@@ -7,7 +7,11 @@
    * Instantiated with immutable type data common to all animations of the same
    * type.
    */
-  function GameAnimation(typeData, state) {
+  function GameAnimation(typeData, frame) {
+    this.typeData = typeData;
+    this.frame = frame || 0;
+    this.program = typeData["default"];
+    this.currentInterval = 0.0;
 /*
     var currentInterval = 0.0;
     var stateIndex = 0;
@@ -36,15 +40,32 @@
   }
 
   GameAnimation.prototype.getFrame = function () {
-    return 0;
+    return this.frame;
   };
 
   GameAnimation.prototype.setProgram = function (identifier) {
-
+    this.program = this.typeData[identifier];
   };
 
   GameAnimation.prototype.update = function (dt) {
+    var doUpdate = false;
 
+    this.currentInterval -= dt;
+    while (this.currentInterval <= 0.0) {
+      if (++this.frame >= this.program.frames.length) {
+        this.frame = 0;
+      }
+      this.currentInterval += this.program.interval;
+      doUpdate = true;
+    }
+    if (doUpdate) {
+      var nextFrame = this.program.frames[this.frame];
+      if (nextFrame < 0) {
+        this.frame = this.program.frames[Math.abs(nextFrame)];
+      } else {
+        this.frame = nextFrame;
+      }
+    }
   };
 
   module.exports = GameAnimation;
