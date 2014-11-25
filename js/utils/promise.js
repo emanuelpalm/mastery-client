f575c1e27f90c0105574703a86d6a7f681c9c94f
Emanuel Palm <emanuelpalm@gmail.com>
2014-11-25 21:42:52 +0100

    Added promise utility module.

---
+++ js/utils/promise.js
@@ -0,0 +1,124 @@
+/**
+ * Various convenience functions for dealing with common tasks with promises.
+ *
+ * @module utils/promise
+ */
+(function () {
+  "use strict";
+
+  var Promise = require("promise");
+
+  /**
+   */
+  module.exports = {
+
+    /**
+     * Returns a promise that is fulfilled immediately with given data.
+     */
+    fulfill: function (data) {
+      return new Promise(function (fulfill) {
+        fulfill(data);
+      });
+    },
+
+    /**
+     * Returns a promise that is rejected immediately with given error.
+     */
+    reject: function (error) {
+      return new Promise(function (_, reject) {
+        reject(error);
+      });
+    },
+
+    /**
+     * Races array of promises against each other. The returned promise is
+     * fulfilled with the data of the winner.
+     *
+     * If all promises are rejected in the array given, the returned promise is
+     * rejected with the error of the last rejected promise.
+     */
+    race: function (promises) {
+      return new Promise(function (fulfill, reject) {
+        var pending = promises.length;
+
+        promises.forEach(function (promise) {
+          promise
+            .then(fulfill)
+            .catch(function (error) {
+              pending -= 1;
+              if (pending === 0) {
+                reject(error);
+              }
+            });
+        });
+      });
+    },
+
+    /**
+     * Starves array of promises. The returned promise is fulfilled with promise
+     * which survive the longest.
+     *
+     * If all promises are rejected in the array given, the returned promise is
+     * rejected with the error of the last rejected promise.
+     */
+    starve: function (promises) {
+      return new Promise(function (fulfill, reject) {
+        var pending = promises.length;
+        var lastData = null, lastError;
+
+        promises.forEach(function (promise) {
+          promise
+            .then(function (data) {
+              lastData = data;
+              completeJob();
+            })
+            .catch(function (error) {
+              lastError = error;
+              completeJob();
+            });
+        });
+
+        function completeJob() {
+          pending -= 1;
+          if (pending === 0) {
+            if (lastData) {
+              fulfill(lastData);
+            } else {
+              reject(lastError);
+            }
+          }
+        }
+      });
+    },
+
+    /**
+     * Returns a promise that is fulfilled in the given amount of milliseconds.
+     *
+     * Optionally some data may be given as second argument, which is then
+     * passed when the promise is fulfilled.
+     */
+    timeout: function (timeInMs, data) {
+      return new Promise(function (fulfill) {
+        setTimeout(function () {
+          fulfill(data);
+        }, timeInMs);
+      });
+    },
+
+    /**
+     * Returns a promise that is rejected in the given amount of milliseconds.
+     *
+     * Optionally some error may be given as second argument, which is then
+     * passed when the promise is rejected.
+     */
+    expire: function (timeInMs, error) {
+      return new Promise(function (_, reject) {
+        setTimeout(function () {
+          reject(error);
+        }, timeInMs);
+      });
+    },
+
+  };
+
+}());
---
+++ test/utils/promise.js
@@ -0,0 +1,157 @@
+(function () {
+  "use strict";
+
+  var promise = require("../../js/utils/promise.js");
+
+  exports.testFulfill = function (test) {
+    promise.fulfill("data")
+      .then(function (data) {
+        test.equal(data, "data");
+        test.done();
+      })
+      .catch(function () {
+        test.ok(false);
+        test.done();
+      });
+  };
+
+  exports.testReject = function (test) {
+    promise.reject("error")
+      .then(function () {
+        test.ok(false);
+        test.done();
+      })
+      .catch(function (error) {
+        test.equal(error, "error");
+        test.done();
+      });
+  };
+
+  exports.testRace = function (test) {
+    promise.race([
+      promise.timeout(5, "slow"),
+      promise.timeout(1, "fast"),
+    ])
+    .then(function (data) {
+      test.equal(data, "fast");
+      test.done();
+    })
+    .catch(function () {
+      test.ok(false);
+      test.done();
+    });
+  };
+
+  exports.testRaceOneRejected = function (test) {
+    promise.race([
+      promise.timeout(1, "slow"),
+      promise.reject("rejected"),
+    ])
+    .then(function (data) {
+      test.equal(data, "slow");
+      test.done();
+    })
+    .catch(function () {
+      test.ok(false);
+      test.done();
+    });
+  };
+
+  exports.testRaceAllRejected = function (test) {
+    promise.race([
+      promise.expire(1, "fastest"),
+      promise.expire(5, "slowest"),
+    ])
+    .then(function () {
+      test.ok(false);
+      test.done();
+    })
+    .catch(function (error) {
+      test.equal(error, "slowest");
+      test.done();
+    });
+  };
+
+  exports.testStarve = function (test) {
+    promise.starve([
+      promise.timeout(1, "fast"),
+      promise.timeout(5, "slow"),
+    ])
+    .then(function (data) {
+      test.equal(data, "slow");
+      test.done();
+    })
+    .catch(function () {
+      test.ok(false);
+      test.done();
+    });
+  };
+
+  exports.testStarveOneRejected = function (test) {
+    promise.starve([
+      promise.expire(5, "slow"),
+      promise.timeout(1, "fast"),
+    ])
+    .then(function (data) {
+      test.equal(data, "fast");
+      test.done();
+    })
+    .catch(function () {
+      test.ok(false);
+      test.done();
+    });
+  };
+
+  exports.testStarveAllRejected = function (test) {
+    promise.starve([
+      promise.expire(1, "fast"),
+      promise.expire(5, "slow"),
+    ])
+    .then(function () {
+      test.ok(false);
+      test.done();
+    })
+    .catch(function (error) {
+      test.equal(error, "slow");
+      test.done();
+    });
+  };
+
+  exports.testTimeout = function (test) {
+    test.expect(2);
+    var hasTimedOut = false;
+
+    promise.timeout(1, "timeout")
+      .then(function (data) {
+        test.equal(data, "timeout");
+        hasTimedOut = true;
+      })
+      .catch(function () {
+        test.ok(false);
+      });
+
+    test.equal(hasTimedOut, false);
+    setTimeout(function () {
+      test.done();
+    }, 5);
+  };
+
+  exports.testExpire = function (test) {
+    test.expect(2);
+    var hasExpired = false;
+
+    promise.expire(1, "expire")
+      .then(function () {
+        test.ok(false);
+      })
+      .catch(function (error) {
+        test.equal(error, "expire");
+        hasExpired = true;
+      });
+
+    test.equal(hasExpired, false);
+    setTimeout(function () {
+      test.done();
+    }, 5);
+  };
+}());
