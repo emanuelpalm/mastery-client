a9003a468d181fd04a5ffd24c99d15db6c6f6821
Emanuel Palm <emanuelpalm@gmail.com>
2014-11-26 21:15:28 +0100

    Continued on Button class.

--- js/model/entity/Button.js
+++ js/model/entity/Button.js
@@ -3,15 +3,25 @@
 
   var GameEntity = require("../../game/GameEntity.js");
 
-  function Button(type, onPress) {
+  function Button(type) {
     GameEntity.call(this, type);
-    this.onPress = onPress;
+    this.callback = function () {};
   }
   Button.prototype = Object.create(GameEntity.prototype);
   Button.prototype.constructor = Button;
 
-  Button.prototype.onEvent = function (evt) {
-    console.log(evt);
+  Button.prototype.onPress = function (f) {
+    this.callback = f;
+  };
+
+  Button.prototype.offerEvent = function (evt) {
+    switch (evt.type) {
+    case "mousedown":
+      if (this.intersects(evt.properties)) {
+        this.callback();
+      }
+      break;
+    }
   };
 
   module.exports = Button;
