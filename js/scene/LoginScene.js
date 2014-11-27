f67179b98e282b612be797af494375aefda5560a
Emanuel Palm <emanuelpalm@gmail.com>
2014-11-27 14:58:43 +0100

    LoginScene now properly handles Facebook login.

--- js/scene/LoginScene.js
+++ js/scene/LoginScene.js
@@ -1,49 +1,55 @@
 (function () {
   "use strict";
 
+  var HomeScene = require("../scene/HomeScene.js");
+  var LoaderScene = require("../scene/LoaderScene.js");
   var GameEntity = require("../game/GameEntity.js");
   var Button = require("../model/entity/Button.js");
+
+  var account = require("../model/account.js");
   var FB = require("../model/login/FB.js");
 
   function LoginScene(gameMode) {
     var entities = [];
-    var facebook, developer;
+    var buttonFB, buttonDEV;
 
     this.load = function (assetLoader, done, failed) {
       assetLoader.loadBatch("/assets/batches/login.json")
         .then(function (batch) {
           entities.push(new GameEntity(batch.entities.logo));
-          entities.push(facebook = new Button(batch.entities.facebook));
+          entities.push(buttonFB = new Button(batch.entities.facebook));
           if (gameMode === "debug") {
-            entities.push(developer = new Button(batch.entities.developer));
+            entities.push(buttonDEV = new Button(batch.entities.developer));
           }
           done();
         })
         .catch(failed);
     };
 
-    this.setup = function (toScene, load) {
-      facebook.onPress(function () {
+    this.setup = function (toScene) {
+      buttonFB.onPress(function () {
         FB.login()
-          .then(function (authResponse) {
-            console.log("Login successful.");
-            console.log(authResponse);
+          .then(function (isLoggedIn, authResponse) {
+            if (isLoggedIn) {
+              return account.authenticate(authResponse)
+                .then(function (account) {
+                  toScene(new LoaderScene(new HomeScene(account)));
+                });
+            }
           })
           .catch(function (e) {
-            if (e) {
-              console.log(e.stack); // TODO: Show nice error message to user.
-            }
+            console.log(e.stack); // TODO: Show nice error message to user.
           });
       });
-      if (developer) {
-        developer.onPress(function () {
-          console.log("developer");
+      if (buttonDEV) {
+        buttonDEV.onPress(function () {
+          console.log("buttonDEV");
         });
       }
       return function (evt) {
-        facebook.offerEvent(evt);
-        if (developer) {
-          developer.offerEvent(evt);
+        buttonFB.offerEvent(evt);
+        if (buttonDEV) {
+          buttonDEV.offerEvent(evt);
         }
       };
     };
