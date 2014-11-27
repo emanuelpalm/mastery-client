02cfd977a1ebb75cb10bc22357d789a4e8a829a2
Emanuel Palm <emanuelpalm@gmail.com>
2014-11-27 15:20:11 +0100

    DEV login module implemented.

--- js/model/login/DEV.js
+++ js/model/login/DEV.js
@@ -1,17 +1,22 @@
+/**
+ * Developer mock-up login.
+ *
+ * @module model/login/DEV
+ */
 (function () {
   "use strict";
 
   /**
-   * Developer login.
+   * Logs in a mock-up user.
    *
-   * This login method ought to only be available when running the application
-   * in debug mode locally. It bypasses the requirement of using any actual
-   * authentication and lets a developer use the application with a standard
-   * account.
+   * The function returns a promise that is guaranteed to be fulfilled with a
+   * first value with true, and a second argument identifying the login attempt
+   * as a debug attempt.
    */
-  function Developer() {
+  exports.login = function () {
+    return new Promise(function (fulfill) {
+      fulfill(true, { mode: "debug" });
+    });
+  };
 
-  }
-
-  module.exports = Developer;
 }());
