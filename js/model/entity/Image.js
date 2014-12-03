194d7026689981682db4ecbe8afa5e775b9ba2a2
Emanuel Palm <emanuelpalm@gmail.com>
2014-12-03 07:59:13 +0100

    Added Image class, which inherits GameEntity.

---
+++ js/model/entity/Image.js
@@ -0,0 +1,34 @@
+(function () {
+  "use strict";
+
+  var GameEntity = require("../../game/GameEntity.js");
+
+  /**
+   * Allows an HTMLImageElement to be treated as a GameEntity.
+   *
+   * Image inherits GameEntity.
+   */
+  function Image(element) {
+    GameEntity.call(this, {
+      sprite: {
+        image: element,
+        width: element.width,
+        height: element.height,
+        states: [
+          [0, 0]
+        ]
+      },
+      bounds: {
+        x: 0,
+        y: 0,
+        width: 60,
+        height: 60
+      }
+    });
+    this.callback = function () {};
+  }
+  Image.prototype = Object.create(GameEntity.prototype);
+  Image.prototype.constructor = Image;
+
+  module.exports = Image;
+}());
